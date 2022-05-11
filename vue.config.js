cf5b10714a9fde50a58a761c563621d0b6cab0cf
beark93 <beark93@nhnwetoo.com>
2022-05-11 17:24:53 +0900

    modify api call #3

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     devServer: {
-        proxy: 'https://diablo2.io/dclone_api.php'
+        publicPath: process.env.NODE_ENV === 'production' ? '/' : '/api',
+        proxy: {
+            '/api': {
+                target: 'https://diablo2.io/dclone_api.php',
+                ws: true,
+                changeOrigin: true
+            }
+        }
     },
     transpileDependencies: true
 })
